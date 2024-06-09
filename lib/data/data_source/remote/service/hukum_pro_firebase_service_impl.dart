f79c324047c4422472c42d14ab135f2f33e44eef
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-09 21:23:29 +0700

    update

--- lib/data/data_source/remote/service/hukum_pro_firebase_service_impl.dart
+++ lib/data/data_source/remote/service/hukum_pro_firebase_service_impl.dart
@@ -3,6 +3,7 @@ import 'package:hukum_pro/common/error/data_exception.dart';
 import 'package:hukum_pro/common/helper/util/util_parse_type.dart';
 import 'package:hukum_pro/data/data_source/remote/entity/version_data_remote_entity.dart';
 import 'package:hukum_pro/data/data_source/remote/service/hukum_pro_firebase_service.dart';
+import 'package:hukum_pro/infrastructure/firebase/auth/future_firebase_auth.dart';
 
 final class HukumProFirebaseServiceImpl implements HukumProFirebaseService {
   final FirebaseDatabase _infrastructure;
@@ -11,18 +12,18 @@ final class HukumProFirebaseServiceImpl implements HukumProFirebaseService {
 
   @override
   Future<VersionDataRemoteEntity> getLatestVersion() {
-    throw StateError('message');
+    return FutureFirebaseAuth.ensureFirebaseIsAuthenticated(() async {
+      final data = await _getLatestData(path: 'versions_new/v2');
+      return VersionDataRemoteEntity.fromJson(data);
+    });
   }
 }
 
 extension _HukumProFirebaseServiceImpl on HukumProFirebaseServiceImpl {
   @override
-  Future<VersionDataRemoteEntity> _getLatestVersion() async {
-    final snapshot = await _infrastructure
-        .ref('versions_new/v2')
-        .orderByKey()
-        .limitToLast(1)
-        .get();
+  Future<Map<String, Object?>> _getLatestData({required String path}) async {
+    final snapshot =
+        await _infrastructure.ref(path).orderByKey().limitToLast(1).get();
 
     if (!snapshot.exists) {
       throw const DataException.noData();
@@ -43,7 +44,10 @@ extension _HukumProFirebaseServiceImpl on HukumProFirebaseServiceImpl {
     if (resultMap == null) {
       throw const FormatException('Failed to parse data');
     }
+    if (!resultMap.containsKey('id')) {
+      resultMap['id'] = key;
+    }
 
-    return VersionDataRemoteEntity.fromJsonWithId(resultMap, key);
+    return resultMap;
   }
 }
