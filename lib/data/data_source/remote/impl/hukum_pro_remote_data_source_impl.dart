cfee9b330f3500b2deab67b72f96d324f13d0885
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-09 21:29:37 +0700

    update

--- lib/data/data_source/remote/impl/hukum_pro_remote_data_source_impl.dart
+++ lib/data/data_source/remote/impl/hukum_pro_remote_data_source_impl.dart
@@ -1,10 +1,11 @@
 import 'package:hukum_pro/data/data_source/remote/entity/version_data_remote_entity.dart';
+import 'package:hukum_pro/data/data_source/remote/impl/hukum_pro_remote_data_source.dart';
 import 'package:hukum_pro/data/data_source/remote/service/hukum_pro_firebase_service.dart';
 
-final class HukumProRemoteDataSource implements HukumProFirebaseService {
+final class HukumProRemoteDataSourceImpl implements HukumProRemoteDataSource {
   final HukumProFirebaseService _firebase;
 
-  HukumProRemoteDataSource(this._firebase);
+  HukumProRemoteDataSourceImpl(this._firebase);
 
   @override
   Future<VersionDataRemoteEntity> getLatestVersion() {
