5cceade9f47b289d433c1b0e0ac67cbab10bd43e
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-09 20:50:50 +0700

    add firebase database

--- lib/infrastructure/di/di_firebase.dart
+++ lib/infrastructure/di/di_firebase.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:hukum_pro/infrastructure/di/di.dart';
 
 class DiFirebase {
   static void setup() {
     di.registerLazySingleton<FirebaseAuth>(() => FirebaseAuth.instance);
+    di.registerLazySingleton<FirebaseDatabase>(() => FirebaseDatabase.instance);
   }
 }
