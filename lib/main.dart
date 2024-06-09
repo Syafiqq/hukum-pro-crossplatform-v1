314afc589563e5d7aa0c4b88345fe60f4e02a92d
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-09 10:29:42 +0700

    001 add flutterfire

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:hukum_pro/infrastructure/firebase/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
