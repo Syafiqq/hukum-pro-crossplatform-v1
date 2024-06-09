deb8a28e06ab91af8a533476a768a60986249873
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-09 14:56:16 +0700

    003: Add firebase auth service

---
+++ lib/infrastructure/firebase/auth/future_firebase_auth.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hukum_pro/infrastructure/di/di.dart';
+import 'package:hukum_pro/infrastructure/firebase/auth/firebase_user_auth_service.dart';
+import 'package:retry/retry.dart';
+
+extension FutureFirebaseAuth on Future {
+  static Future<T> ensureFirebaseIsAuthenticated<T>(
+    FutureOr Function() fn, {
+    bool forceAuthenticated = false,
+  }) async {
+    return retry(
+      () async {
+        await _handleFirebaseAuthentication(forceAuthenticated);
+        return await fn();
+      },
+      maxAttempts: 2,
+      retryIf: (e) {
+        return e is FirebaseException &&
+            (e.code == 'user-mismatch' || e.code == 'permission-denied');
+      },
+      onRetry: (_) => _handleFirebaseAuthentication(true),
+    );
+  }
+
+  static Future _handleFirebaseAuthentication(bool forceAuthenticated) {
+    return retry(
+      () => di<FirebaseUserAuthService>().ensureUserIsAuthenticated(
+        forceAuthenticated: forceAuthenticated,
+      ),
+      maxAttempts: 2,
+    );
+  }
+}
