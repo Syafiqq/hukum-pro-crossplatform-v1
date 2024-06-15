badeb50060f4a9e2d2df9bd63d188b63014324fd
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-15 18:43:38 +0700

    005: CDS, Add HukumPro Pref Service

--- lib/data/di/di_data.dart
+++ lib/data/di/di_data.dart
@@ -1,7 +1,9 @@
+import 'package:hukum_pro/data/di/di_datasource_cache.dart';
 import 'package:hukum_pro/data/di/di_datasource_remote.dart';
 
 class DiData {
   static void setup() {
     DiDatasourceRemote.setup();
+    DiDatasourceCache.setup();
   }
 }
