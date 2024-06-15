6c7bcfb65446f23f6489b752dae0809c6b46cc4f
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-16 05:59:13 +0700

    005: LDS, add isar lds

--- lib/data/di/di_datasource_local.dart
+++ lib/data/di/di_datasource_local.dart
@@ -1,3 +1,5 @@
+import 'package:hukum_pro/data/data_source/local/impl/hukum_pro_local_data_source.dart';
+import 'package:hukum_pro/data/data_source/local/impl/hukum_pro_local_data_source_impl.dart';
 import 'package:hukum_pro/data/data_source/local/service/hukum_pro_isar_service.dart';
 import 'package:hukum_pro/data/data_source/local/service/hukum_pro_isar_service_impl.dart';
 import 'package:hukum_pro/data/data_source/local/service/hukum_pro_path_service.dart';
@@ -7,6 +9,7 @@ import 'package:hukum_pro/infrastructure/di/di.dart';
 class DiDatasourceLocal {
   static void setup() {
     _setupService();
+    _setupDataSource();
   }
 
   static void _setupService() {
@@ -17,6 +20,12 @@ class DiDatasourceLocal {
       () => HukumProIsarServiceImpl(
         di.getAsync(),
       ),
+    );
+  }
+
+  static void _setupDataSource() {
+    di.registerLazySingleton<HukumProLocalDataSource>(
+      () => HukumProLocalDataSourceImpl(di()),
     );
   }
 }
