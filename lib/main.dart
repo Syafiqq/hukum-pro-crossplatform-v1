94100953b1db950238652bb973f300a1277d6687
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-15 17:18:01 +0700

    --dev-only

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hukum_pro/data/data_source/remote/impl/hukum_pro_remote_data_source.dart';
 import 'package:hukum_pro/di.dart';
 import 'package:hukum_pro/infrastructure/di/di.dart';
 import 'package:hukum_pro/infrastructure/firebase/auth/firebase_user_monitor_service.dart';
@@ -16,4 +17,15 @@ void main() async {
 
   // Firebase Auth
   di<FirebaseUserMonitorService>().start();
+
+  di<HukumProRemoteDataSource>()
+      .getLatestVersion()
+      .then((value) {
+        // print all value properties
+        print('CurrentLog - id - ${value.id}');
+        print('CurrentLog - milis - ${value.milis}');
+        print('CurrentLog - version - ${value.version}');
+        print('CurrentLog - timestamp - ${value.timestamp}');
+        print('CurrentLog - detail - ${value.detail}');
+  });
 }
