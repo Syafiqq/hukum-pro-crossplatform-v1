9ff6384811125bd5ca9bc83b6ff5faeda6eb7839
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-16 05:31:38 +0700

    006: LDS, add law menu entity

--- lib/infrastructure/di/di_isar.dart
+++ lib/infrastructure/di/di_isar.dart
@@ -1,3 +1,4 @@
+import 'package:hukum_pro/data/data_source/local/entity/law_menu_order_data_local_entity.dart';
 import 'package:hukum_pro/data/data_source/local/service/hukum_pro_path_service.dart';
 import 'package:hukum_pro/infrastructure/di/di.dart';
 import 'package:isar/isar.dart';
@@ -8,6 +9,7 @@ class DiIsar {
       () async {
         final directory = await di<HukumProPathService>().getLocalDbDirectory();
         final isar = Isar.openAsync(schemas: [
+          LawMenuOrderDataLocalEntitySchema
         ], directory: directory.path);
         return isar;
       },
