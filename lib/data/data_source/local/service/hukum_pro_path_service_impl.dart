9c2e4a74b8879c8e48ca0a99de7068a4c5173d4b
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-16 04:11:12 +0700

    add path service

---
+++ lib/data/data_source/local/service/hukum_pro_path_service_impl.dart
@@ -0,0 +1,5 @@
+import 'package:hukum_pro/data/data_source/local/service/hukum_pro_path_service.dart';
+
+final class HukumProPathServiceImpl implements HukumProPathService {
+  HukumProPathServiceImpl();
+}
