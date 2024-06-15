3e9834ae201c409352ee60af11a9543922d75ab6
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-16 05:59:46 +0700

    006: LDS, add law menu lds

--- lib/data/data_source/local/impl/hukum_pro_local_data_source_impl.dart
+++ lib/data/data_source/local/impl/hukum_pro_local_data_source_impl.dart
@@ -1,3 +1,4 @@
+import 'package:hukum_pro/data/data_source/local/entity/law_menu_order_data_local_entity.dart';
 import 'package:hukum_pro/data/data_source/local/impl/hukum_pro_local_data_source.dart';
 import 'package:hukum_pro/data/data_source/local/service/hukum_pro_isar_service.dart';
 
@@ -5,4 +6,9 @@ final class HukumProLocalDataSourceImpl implements HukumProLocalDataSource {
   final HukumProIsarService _isar;
 
   HukumProLocalDataSourceImpl(this._isar);
+
+  @override
+  Future<void> replaceLawMenus(List<LawMenuOrderDataLocalEntity> menus) {
+    return _isar.replaceLawMenus(menus);
+  }
 }
