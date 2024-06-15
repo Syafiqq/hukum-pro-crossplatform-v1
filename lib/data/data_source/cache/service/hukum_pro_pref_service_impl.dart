27d77c47c2a91c1c1f4ea6fe852131088a3ec269
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-15 18:43:38 +0700

    add pref

---
+++ lib/data/data_source/cache/service/hukum_pro_pref_service_impl.dart
@@ -0,0 +1,29 @@
+import 'dart:async';
+
+import 'package:hukum_pro/common/helper/future/future_throttled.dart';
+import 'package:hukum_pro/data/data_source/cache/service/hukum_pro_pref_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+final class HukumProPrefServiceImpl implements HukumProPrefService {
+  final Future<SharedPreferences> _futureInfrastructure;
+  SharedPreferences? _readOnlyPrefs;
+
+  final FutureThrottled<SharedPreferences> _fetcher =
+      FutureThrottled<SharedPreferences>();
+
+  FutureOr<SharedPreferences> get _infrastructure {
+    if (_readOnlyPrefs != null) {
+      return _readOnlyPrefs!;
+    }
+    return _fetcher.execute(
+      _futureInfrastructure.then(
+        (value) {
+          _readOnlyPrefs = value;
+          return value;
+        },
+      ),
+    );
+  }
+
+  HukumProPrefServiceImpl(this._futureInfrastructure);
+}
