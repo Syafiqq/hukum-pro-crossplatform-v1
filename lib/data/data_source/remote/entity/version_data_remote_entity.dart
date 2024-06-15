587e6080b270bc1ffca74238be463011a3a70022
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-15 15:41:14 +0700

    update version data remote entity

--- lib/data/data_source/remote/entity/version_data_remote_entity.dart
+++ lib/data/data_source/remote/entity/version_data_remote_entity.dart
@@ -4,22 +4,19 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'version_data_remote_entity.g.dart';
 
-@JsonSerializable(fieldRename: FieldRename.snake)
+@JsonSerializable(fieldRename: FieldRename.snake, anyMap: true)
 final class VersionDataRemoteEntity {
-  @IntOrStringToIntConverter()
-  final int? id;
+  @StringOrIntToStringConverter()
+  final String? id;
 
   @IntOrStringToIntConverter()
   final int? milis;
 
-  @StringOrIntToStringConverter()
   final String? timestamp;
 
-  VersionDataRemoteEntity({
-    this.id,
-    this.milis,
-    this.timestamp,
-  });
+  final VersionDetailDataRemoteEntity? detail;
+
+  VersionDataRemoteEntity({this.id, this.milis, this.timestamp, this.detail});
 
   factory VersionDataRemoteEntity.fromJson(
     Map<String, dynamic> json,
@@ -27,4 +24,49 @@ final class VersionDataRemoteEntity {
       _$VersionDataRemoteEntityFromJson(json);
 
   Map<String, dynamic> toJson() => _$VersionDataRemoteEntityToJson(this);
+}
+
+@JsonSerializable(fieldRename: FieldRename.snake, anyMap: true)
+final class VersionDetailDataRemoteEntity {
+  @JsonKey(defaultValue: [])
+  final List<String> lawFilenames;
+
+  @JsonKey(defaultValue: [])
+  final List<VersionOrderDataRemoteEntity> orders;
+
+  VersionDetailDataRemoteEntity({
+    required this.lawFilenames,
+    required this.orders,
+  });
+
+  factory VersionDetailDataRemoteEntity.fromJson(
+    Map<String, dynamic> json,
+  ) =>
+      _$VersionDetailDataRemoteEntityFromJson(json);
+
+  Map<String, dynamic> toJson() => _$VersionDetailDataRemoteEntityToJson(this);
+}
+
+@JsonSerializable(fieldRename: FieldRename.snake, anyMap: true)
+final class VersionOrderDataRemoteEntity {
+  @StringOrIntToStringConverter()
+  final String? id;
+
+  @IntOrStringToIntConverter()
+  final int? order;
+
+  final String? name;
+
+  VersionOrderDataRemoteEntity({
+    this.id,
+    this.order,
+    this.name,
+  });
+
+  factory VersionOrderDataRemoteEntity.fromJson(
+    Map<String, dynamic> json,
+  ) =>
+      _$VersionOrderDataRemoteEntityFromJson(json);
+
+  Map<String, dynamic> toJson() => _$VersionOrderDataRemoteEntityToJson(this);
 }
