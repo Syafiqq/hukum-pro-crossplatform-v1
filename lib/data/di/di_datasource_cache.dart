789a3cc1c1239d9a66a90113b2bf113697a3df6a
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-15 18:43:38 +0700

    005: CDS, Add HukumPro Pref Service

---
+++ lib/data/di/di_datasource_cache.dart
@@ -0,0 +1,15 @@
+import 'package:hukum_pro/data/data_source/cache/service/hukum_pro_pref_service.dart';
+import 'package:hukum_pro/data/data_source/cache/service/hukum_pro_pref_service_impl.dart';
+import 'package:hukum_pro/infrastructure/di/di.dart';
+
+class DiDatasourceCache {
+  static void setup() {
+    _setupService();
+  }
+
+  static void _setupService() {
+    di.registerLazySingleton<HukumProPrefService>(
+      () => HukumProPrefServiceImpl(di()),
+    );
+  }
+}
