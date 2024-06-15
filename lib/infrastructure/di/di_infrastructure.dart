5e5e5fbdb0c15ed8e2a7f2cf59ae19f7dd530b44
msyafiqq <muhammad.syafiq@geniebook.com>
2024-06-15 18:35:44 +0700

    005: Dependency, add Shared PReference

--- lib/infrastructure/di/di_infrastructure.dart
+++ lib/infrastructure/di/di_infrastructure.dart
@@ -1,9 +1,12 @@
+import 'package:hukum_pro/infrastructure/di/di.dart';
 import 'package:hukum_pro/infrastructure/di/di_firebase.dart';
 import 'package:hukum_pro/infrastructure/di/di_firebase_service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DiInfrastructure {
   static void setup() {
     DiFirebase.setup();
     DiFirebaseService.setup();
+    di.registerLazySingleton(() => SharedPreferences.getInstance());
   }
 }
